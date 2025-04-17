988ba77b5a05a5fab26826645b6af381b85b1f16
thelimonadee <142876672+thelimonadee@users.noreply.github.com>
2025-04-16 21:57:25 -0700

    Update config.php 4/16

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 // config.php
 // Chapter 16 reference- Setup for backend file and path management
+// Similar to prior config files
+
 // Base path of application 
 // located in /opt/lampp/htdocs/project for me- using linux machine
 $BASE_PATH = $_SERVER['DOCUMENT_ROOT'] . '/project';
 
-// Public HTML/PHP pages live in base for now
+// Public HTML/PHP pages = base
 $DOC_PATH = $BASE_PATH;
 
 // Private data (ex: logs, uploads, or json) could go here 
