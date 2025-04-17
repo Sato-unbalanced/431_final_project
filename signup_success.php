9e5274e6efc177042d948e4449725bba0a1e6f18
thelimonadee <142876672+thelimonadee@users.noreply.github.com>
2025-04-16 22:00:19 -0700

    Create signup_success.php

---
+++ signup_success.php
@@ -0,0 +1,24 @@
+<?php
+// signup_success.php
+// Confirms success and links to login
+require_once('config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Signup Success</title>
+  <style>
+    body {
+      font-family: Arial;
+      text-align: center;
+      padding: 5rem;
+    }
+  </style>
+</head>
+<body>
+  <h2>Account created successfully!!!</h2>
+  <p>You may now <a href="login_form.php">log in</a>.</p>
+</body>
+</html>
